5525e853ca336704ee382bfdd9f8d0e2dadad7ee
Syvra <66771495+Syvraa@users.noreply.github.com>
2026-03-09 00:00:51 +0100

    fix: sign extend immediates in add, sub, xor and cmp

---
+++ tests/cmp.rs
@@ -0,0 +1,35 @@
+use std::arch::asm;
+
+use s86::Simulator;
+
+#[test]
+fn sign_extend() {
+    let expected: u64;
+    unsafe {
+        asm!(
+            "mov rax, -3",
+            "cmp rax, -3",
+            "je 2f",
+            "mov rax, 1",
+            "jmp 3f",
+            "2:",
+            "mov rax, 2",
+            "3:",
+            out("rax") expected);
+    }
+
+    let source = "
+    mov rax, -3
+    cmp rax, -3
+    je good
+    mov rax, 1
+    jmp end
+    good:
+    mov rax, 2
+    end:
+";
+    let mut simulator = Simulator::new(source);
+    simulator.run();
+
+    assert_eq!(simulator.registers.rax, expected);
+}
--- tests/xor.rs
+++ tests/xor.rs
@@ -36,3 +36,23 @@ fn xor_reg() {
 
     assert_eq!(simulator.registers.rcx, expected);
 }
+
+#[test]
+fn sign_extend() {
+    let expected: u64;
+    unsafe {
+        asm!(
+            "mov rax, -8",
+            "xor rax, -3",
+            out("rax") expected);
+    }
+
+    let source = "
+    mov rax, -8
+    xor rax, -3
+";
+    let mut simulator = Simulator::new(source);
+    simulator.run();
+
+    assert_eq!(simulator.registers.rax, expected);
+}
