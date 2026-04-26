31d100e5059b89e46b3fb3f5ebf1ca4e3e50beca
Syvra <66771495+Syvraa@users.noreply.github.com>
2026-04-26 02:20:54 +0200

    feat(web): change rendered bytes per row based on screen width

--- web/src/lib/Memory.svelte
+++ web/src/lib/Memory.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import { StateDiff } from "s86-lib";
+  import { MediaQuery } from "svelte/reactivity";
 
   let { diff }: { diff: StateDiff } = $props();
 
   let memory: number[] = $state([]);
+  let bytesPerRow = $derived(new MediaQuery("min-width: 1280px").current ? 32 : 16);
 
   $effect(() => {
     for (const mem_diff of diff.mem_diffs) {
@@ -21,14 +23,18 @@
   }
 </script>
 
-<div class="memory" style:grid-template-rows="repeat({Math.ceil(memory.length / 32) + 1}, 1em)">
+<div
+  class="memory"
+  style:grid-template-rows="repeat({Math.ceil(memory.length / bytesPerRow) + 1}, 1em)"
+  style:grid-template-columns="auto repeat({bytesPerRow}, 1fr) auto"
+>
   <div class="header" style:grid-column="1">Addr</div>
-  <div class="header" style:grid-column="2 / span 32">Data</div>
-  <div class="header" style:grid-column="34">Addr</div>
+  <div class="header" style:grid-column="2 / span {bytesPerRow}">Data</div>
+  <div class="header" style:grid-column={bytesPerRow + 2}>Addr</div>
   {#each memory as byte, i (i)}
-    {@const row = Math.floor(i / 32) + 2}
-    {@const col = (i % 32) + 2}
-    {#if i % 32 === 0}
+    {@const row = Math.floor(i / bytesPerRow) + 2}
+    {@const col = (i % bytesPerRow) + 2}
+    {#if i % bytesPerRow === 0}
       <div class="address" style:grid-row={row} style:grid-column={1}>0x{i.toString(16)}</div>
     {/if}
     <div
@@ -39,8 +45,10 @@
     >
       {byte.toString(16)}
     </div>
-    {#if i % 32 === 31}
-      <div class="address" style:grid-row={row} style:grid-column={34}>0x{i.toString(16)}</div>
+    {#if i % bytesPerRow === bytesPerRow - 1}
+      <div class="address" style:grid-row={row} style:grid-column={bytesPerRow + 2}>
+        0x{i.toString(16)}
+      </div>
     {/if}
   {/each}
 </div>
@@ -49,7 +57,6 @@
   .memory {
     width: 100%;
     display: grid;
-    grid-template-columns: auto repeat(32, 1fr) auto;
     flex: 1;
     overflow-y: scroll;
     font-family: monospace;
